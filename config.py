080c1fa6179ed111304baea68b3ab04b6e5e6fc1
Reza-ashrafi <115076971+Reza-ashrafi@users.noreply.github.com>
2026-07-06 09:06:24 +0330

    Add configuration variables for bot and API

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
+CHAT_ID = "PUT_YOUR_CHAT_ID_HERE"
+
+SILVER_API = "https://api.metals.live/v1/spot/silver"
+
+MARKET_PREMIUM = 1.0  # فعلاً ساده، بدون پیچیدگی
+REQUEST_TIMEOUT = 10
