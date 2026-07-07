29197525ebe4d45af1c31b026692e7e960883c62
Reza-ashrafi <115076971+Reza-ashrafi@users.noreply.github.com>
2026-07-07 21:33:11 +0330

    Update tsetmc_client.py

--- collectors/tsetmc_client.py
+++ collectors/tsetmc_client.py
@@ -8,22 +8,15 @@ class TSETMCClient:
     def __init__(self):
         self.timeout = REQUEST_TIMEOUT
 
+
     def get(self, url):
+
+        print("TSETMC REQUEST START")
+
         last_error = None
 
-        for _ in range(RETRY_COUNT):
+        for i in range(RETRY_COUNT):
+
             try:
-                response = requests.get(
-                    url,
-                    timeout=self.timeout
-                )
 
-                if response.status_code == 200:
-                    return response.text
-
-            except Exception as e:
-                last_error = e
-
-        raise Exception(
-            f"TSETMC connection failed: {last_error}"
-        )
+                print(f"TRY {i+1}")
